55d5dc915acf65533c85787d3ef538f1e6e99a7a
Laszlo Sandor <voidlaszlo@gmail.com>
2019-11-07 14:33:21 +0100

    when render, scrollintoview()

--- webshop/assets/app/app.js
+++ webshop/assets/app/app.js
@@ -77,6 +77,7 @@ class App {
         this.shoeContainer.render()
         this.shoeContainer.filterShoes()
         this.inputManager = new InputManager()
+        document.querySelector('header').scrollIntoView()
     }
 }
 
